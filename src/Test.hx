8e7dbcbfbd4159e5e447e341cd7c086aa17afffb
Mike Almond <mikedotalmond@googlemail.com>
2012-10-13 18:56:29 +0100

    added some mouse control to the vertex shader test

--- src/Test.hx
+++ src/Test.hx
@@ -3,6 +3,7 @@ import flash.events.Event;
 import flash.events.MouseEvent;
 import flash.geom.Matrix3D;
 import flash.geom.Vector3D;
+import flash.Lib;
 import mikedotalmond.tri.Const;
 import mikedotalmond.tri.PolygonManager;
 
@@ -34,26 +35,26 @@ import net.hires.debug.Stats;
 		
 		var tcolour : Float4; // vertex colour temporary register
 		
-		/**
-		 * 
-		 * @param	mpos	position matrix
-		 * @param	mproj	projection matrix
-		 * @param 	t		time
-		 */
-		function vertex( mpos : M44, mproj : M44, t:Float ) {
+		 /**
+		  * 
+		  * @param	mpos	position matrix
+		  * @param	mproj	projection matrix
+		  * @param 	t		time
+		  * @param	fx		frequency x
+		  * @param	fy		frequency y
+		  * @param	amplitude
+		  */
+		function vertex( mpos : M44, mproj : M44, time:Float, fx:Float, fy:Float, amplitude:Float) {
 			
-			var p 			= pos.xyzw;	
+			var p 		= pos.xyzw;
+			var phase   = (p.x * fx + p.y * fy);
 			
-			var xMix		= 1;
-			var yMix		= 0.667;
-			var phase   	= (p.x * xMix + p.y * yMix);
-			var amplitude 	= 0.33;
+			p.z 		-= amplitude * sin(time + phase);
 			
-			p.z 	-= amplitude * sin(t + phase);
+			out 		= p.xyzw * mpos * mproj;
 			
-			out 	= p.xyzw * mpos * mproj;
-			
-			tcolour = colour;
+			// read vertex colour so we can access it in the fragment
+			tcolour 	= colour;
 		}
 		
 		function fragment() {
@@ -77,6 +78,11 @@ import net.hires.debug.Stats;
 	var positionMatrix:Matrix3D;
 	var stats		:Stats;
 	var polyManager	:PolygonManager;
+	
+	private var stageWidth:Int;
+	private var stageHeight:Int;
+	private var halfStageWidth:Int;
+	private var halfStageHeight:Int;
 
 	function new() {
 		
@@ -94,7 +100,23 @@ import net.hires.debug.Stats;
 
 	function onContextReady( _ ) {
 		
-		context3D = stage3D.context3D;
+		setup3D();
+		
+		stage.addEventListener(Event.RESIZE, onResize);
+		stage.addChild(stats = new Stats());
+		
+		onResize(null);
+	}
+	
+	private function setup3D() {
+		
+		context3D 		= stage3D.context3D;
+		shader 			= new Shader(context3D);
+		polyManager 	= new PolygonManager();
+		positionMatrix 	= new Matrix3D();
+		camera 			= new Camera(60, 1, 1, 0.02, 15);
+		camera.up		= new Vector(0, 1, 0);
+		camera.pos.z 	= 9;
 		
 		#if debug 
 		context3D.enableErrorChecking = true;
@@ -104,25 +126,11 @@ import net.hires.debug.Stats;
 		context3D.configureBackBuffer( stage.stageWidth, stage.stageHeight, 4, false );//antialias=4
 		#end
 		
-		polyManager = new PolygonManager();
-		shader 		= new Shader(context3D);
-		camera 		= new Camera(60, 1, 1, 0.02, 20);
-		camera.up	= new Vector(0, 1, 0);
-		positionMatrix = new Matrix3D();
-		
 		polyManager.init();
 		
 		createPolygons();
 		
 		polyManager.alloc(context3D);
-		
-		stage.showDefaultContextMenu = false;
-		stage.addEventListener(Event.RESIZE, onResize);
-		stage.quality = StageQuality.LOW;
-		stats = new Stats();
-		stage.addChild(stats);
-		
-		onResize(null);
 	}
 	
 	private function createPolygons() {
@@ -148,39 +156,43 @@ import net.hires.debug.Stats;
 		var w:Int = stage.stageWidth;
 		var h:Int = stage.stageHeight;
 		
-		camera.ratio = w / h;
-		camera.update();
+		stageWidth  	= w;
+		stageHeight 	= h;
+		halfStageWidth 	= w >> 1;
+		halfStageHeight = h >> 1;
 		
-		//polyManager.setStageSize(w,h);
-		
+		// update ui
 		stats.x = w - stats.width;
 		stats.y = h - stats.height;
+		
+		// update scene
+		camera.ratio = w / h;
+		camera.update();
 	}
 
 	function update(_) {
 		if( context3D == null ) return;
 		
-		t += 0.05;
-		
 		context3D.clear(0, 0, 0, 1);
 		
+		// allow alpha blending
 		context3D.setBlendFactors(Context3DBlendFactor.SOURCE_ALPHA, Context3DBlendFactor.ONE_MINUS_SOURCE_ALPHA);
+		// no depth test for now...
 		context3D.setDepthTest( false, Context3DCompareMode.LESS_EQUAL );
-		context3D.setCulling(Context3DTriangleFace.NONE);
+		// don't draw back-faces
+		context3D.setCulling(Context3DTriangleFace.BACK);
 		
-		//camera.update();
 		
-		var project = camera.m.toMatrix();
 		var pm = polyManager;
 		var poly = cast(pm.polyZero, RadialPolygon);
 		while (poly != null) {
 			//poly.rotation = Math.cos(t + poly.index % 13);
 			//poly.translate(Math.sin(t)*0.0005*(poly.index%7),Math.cos(t/2)*0.0005*(poly.index%5),0);// = Math.cos(t + poly.index % 13);
-			poly.outerColour.x += (Math.random() * 0.5 - poly.outerColour.x) * 0.5;
-			poly.outerColour.y += (Math.random() * 0.5 - poly.outerColour.y) * 0.5;
+			poly.outerColour.x += (Math.random() * 0.6 - poly.outerColour.x) * 0.5;
+			poly.outerColour.y += (Math.random() * 0.6 - poly.outerColour.y) * 0.5;
 			//poly.outerColour.z += (Math.random() * 0.5 - poly.outerColour.z) * 0.5;
 			//poly.innerColour.set(Math.random(), Math.random(), Math.random(), 0);
-			poly.scale = 0.08 + Math.sin(t+poly.index) * 0.01;// * Math.cos(t - 1 / t * t);
+			//poly.scale = 0.08 + Math.sin(t+poly.x*poly.y) * 0.01;// * Math.cos(t - 1 / t * t);
 			poly.updateVertexBuffer();
 			
 			poly = cast poly.next;
@@ -192,9 +204,24 @@ import net.hires.debug.Stats;
 		//positionMatrix.identity();
 		//positionMatrix.appendRotation(t * 10, flash.geom.Vector3D.Z_AXIS);
 		
+		var project = camera.m.toMatrix();
+		
+		var mouseX:Float = (stage.mouseX / stageWidth) - 0.5;
+		var mouseY:Float = (stage.mouseY / stageHeight) - 0.5;
+		
+		t += 0.1 + 0.1 * mouseX;
+		
+		camera.zoom = 1.05 + mouseX * 0.1;
+		camera.update();
 		
 		shader.init(
-			{ mpos : positionMatrix, mproj : project, t:t },
+			{	mpos		: positionMatrix, 
+				mproj		: project, 
+				time		: t,
+				fx			: mouseX * 2, 
+				fy			: mouseY * 2, 
+				amplitude	: 0.2 + mouseY
+			},
 			{  }
 		);
 		
@@ -204,7 +231,12 @@ import net.hires.debug.Stats;
 	}
 
 	static function main() {
+		
+		Lib.current.stage.quality = StageQuality.LOW;
+		Lib.current.stage.showDefaultContextMenu = false;
+		
 		haxe.Log.setColor(0xFF0000);
+		
 		var inst = new Test();
 	}
 }
